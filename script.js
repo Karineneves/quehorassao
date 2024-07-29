86b052327f27dd5ca72f45e7f4b0a3725f738baa
Karine Neves <karineoneves.96@gmail.com>
2024-07-29 11:38:23 -0300

    Projetofinal

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function carregar(){
 var msg = document.getElementById ('msg')
 var img = document.getElementById ('imagem')
-//var data = new Date ()
-var hora=8
+var data = new Date ()
 var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas`
 if (hora >= 0 && hora <12){
     //bom dia
-    msg.innerHTML = 'Bom dia'
     img.src = 'foto-dia.png'
     document.body.style.backgroundColor = '#e5d450'
 } else if(hora >= 12 && hora <= 18) {
